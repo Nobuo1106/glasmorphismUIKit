b77b4078f517f3592241427f70a0ddcee3151120
五十嵐伸雄 <gonnbe1106@gmail.com>
2024-01-23 07:57:39 +0900

    Initial Commit

---
+++ Design+codeUIKit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design+codeUIKit1
+//
+//  Created by 五十嵐伸雄 on 2024/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
