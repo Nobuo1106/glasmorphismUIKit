20fa54b733209bd4337ebef613bdd521e39df86c
五十嵐伸雄 <gonnbe1986@gmail.com>
2024-02-18 11:34:27 +0900

    added loginCardView Animation

--- Design+codeUIKit1/ViewController/LoginViewController.swift
+++ Design+codeUIKit1/ViewController/LoginViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginCard: CustomView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var accessoryButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIView.animate(withDuration: 0.25, delay: 0.5, options: .curveEaseInOut) {
+            self.loginCard.alpha = 1
+            self.loginCard.frame = self.loginCard.frame.offsetBy(dx: 0, dy: -400)
+        }
     }
 }
