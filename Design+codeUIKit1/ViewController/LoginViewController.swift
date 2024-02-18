c8aecaa17e2ea294ee2d5f8d56f9ec46bffc21fd
五十嵐伸雄 <gonnbe1986@gmail.com>
2024-02-18 10:50:30 +0900

    Added LoginViewController UI

---
+++ Design+codeUIKit1/ViewController/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Design+codeUIKit1
+//
+//  Created by 五十嵐伸雄 on 2024/02/18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
